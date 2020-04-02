29c507c9f39880717ace1eccefb4d57748f9bd07
Tyler Eddy <tylereddy96@gmail.com>
2020-04-02 15:00:11 +0100

    changed to work with gcp db

--- model/__init__.py
+++ model/__init__.py
@@ -2,5 +2,5 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 
 #database+driver://username:password@ipaddress:port/database
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:backendboys@34.76.46.2141:3306/note"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:backendboys@34.76.46.214:3306/note"
 db = SQLAlchemy(app)
