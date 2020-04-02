3aa94a1ee1a2d89e13b2b68bb80cfbc0902bb474
Tyler Eddy <tylereddy96@gmail.com>
2020-04-02 15:48:44 +0100

    added notebook id to the note

--- model/note.py
+++ model/note.py
@@ -3,3 +3,5 @@ from model import db
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(60), nullable=False)
+    notebook_id = db.Column(db.Integer, db.ForeignKey("notebook.id"), nullable=False)
+    notebook = db.relationship("Notebook", back_populates="notes")
