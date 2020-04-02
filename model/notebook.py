67deb2336c1e57b92ac815330b3228e376219603
ttiyemba <tapiwatiyemba@icloud.com>
2020-04-02 16:51:50 +0100

    note and notebook tables created and also bug fixed an error in the notebook model

--- model/notebook.py
+++ model/notebook.py
@@ -1,7 +1,7 @@
 from model import db
 
 
-class Notebook(db.model):
+class Notebook(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     notes = db.relationship("Note", cascade="all,delete", back_populates="notebook")
