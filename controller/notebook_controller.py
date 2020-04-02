ff9e233d022ab0edbc599a71c63f7487e8415180
Haziq Mahmood <haziqmahmood1998@gmail.com>
2020-04-02 17:36:12 +0100

    added post functionality for notebook

--- controller/notebook_controller.py
+++ controller/notebook_controller.py
@@ -10,3 +10,12 @@ def view__single_notebook(notebook_id):
     notebook = db.session.query(Note).filter(Note.notebook_id == notebook_id).first()
     notebook_title = db.session.query(Notebook).filter(Notebook.id == notebook_id).first()
     return render_template("notebook.html", title=notebook_title.title, notebook = notebook, current_notebook = notebook_title)
+
+
+@app.route("/notebook", methods=["POST"])
+def add_notebook():
+    notebook_data = request.form
+    notebook = Notebook(**notebook_data)
+    db.session.add(notebook)
+    db.session.commit()
+    return redirect( url_for ( "get_notebooks" ) ) 
